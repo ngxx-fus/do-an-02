6aab7f9454e4b833391eb18e909f4294e3cae875
FIXED-TERM Nguyen Thanh Phu (MS/EAE22-XC) <pgu7hc@bosch.com>
2025-12-04 14:11:09 +0700

    add AppFont

---
+++ AppFonts/AppFont.h
@@ -0,0 +1,31 @@
+#ifndef __FONT_UTILS_H__
+#define __FONT_UTILS_H__
+
+#include "gfxfont.h"
+<<<<<<< HEAD
+#include "localFonts/FreeSerifBoldItalic24pt7b.h"
+#include "localFonts/FreeSerifBoldItalic18pt7b.h"
+#include "localFonts/FreeSerifBoldItalic12pt7b.h"
+#include "localFonts/FreeSerifBoldItalic9pt7b.h"
+#include "localFonts/FreeSerif9pt7b.h"
+#include "localFonts/Picopixel.h"
+=======
+#include "localFonts/FreeSerif9pt7b.h"
+>>>>>>> master
+
+#ifndef defaultTextH
+    #define defaultTextH(__GFXfont) (__GFXfont.yAdvance)
+#endif
+
+extern GFXfont    mainFont;
+<<<<<<< HEAD
+extern GFXfont    fontTitle;
+extern GFXfont    fontBody;
+extern GFXfont    fontHeading01;
+extern GFXfont    fontHeading02;
+extern GFXfont    fontHeading03;
+extern GFXfont    fontNote;
+=======
+>>>>>>> master
+
+#endif 
